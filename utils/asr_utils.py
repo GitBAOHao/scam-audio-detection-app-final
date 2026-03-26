6ffdb762ff395a96c1cf8e92a2ec097f90ada8ec
GitBAOHao <22andybao22@gmail.com>
2026-03-26 22:04:59 +0800

    Update asr_utils.py

--- utils/asr_utils.py
+++ utils/asr_utils.py
@@ -14,7 +14,6 @@ def load_asr_model():
 def transcribe_audio(uploaded_file):
     load_asr_model()
 
-    # 把上传文件暂存成一个临时 flac 文件
     with tempfile.NamedTemporaryFile(delete=False, suffix=".flac") as temp_audio:
         temp_audio.write(uploaded_file.read())
         temp_audio_path = temp_audio.name
